e8cbd59448396d7850f0acda77e9d3fc191a1384
Nicolo Micheletti <michelettinik@gmail.com>
2026-07-17 17:11:51 +0800

    Drop coupled zslMfnr from Android DeviceManager photo path

--- mobile/modules/bluetooth-sdk/android/src/main/java/com/mentra/bluetoothsdk/DeviceManager.kt
+++ mobile/modules/bluetooth-sdk/android/src/main/java/com/mentra/bluetoothsdk/DeviceManager.kt
@@ -1620,7 +1620,6 @@ class DeviceManager {
         live.sendButtonPhotoSettings(
             requestId,
             settings.size?.value,
-            settings.resolvedZslMfnr(),
             settings.mfnr,
             settings.zsl,
             settings.noiseReduction,
