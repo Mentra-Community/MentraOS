11bc92a9f4f4cf204b075cece5771e88a155fad7
Alex Israelov <alex1115alex@gmail.com>
2026-07-22 16:55:51 -0700

    Bundle Mentra Notes 1.0.13

--- mobile/src/generated/bundledMiniapps.ts
+++ mobile/src/generated/bundledMiniapps.ts
@@ -11,7 +11,7 @@ export const BUNDLED_MINIAPPS: number[] = [
   require("@assets/miniapps/com.mentra.livestreamer-1.0.7.zip"),
   require("@assets/miniapps/com.mentra.merge-0.1.29.zip"),
   require("@assets/miniapps/com.mentra.navigation-1.1.24.zip"),
-  require("@assets/miniapps/com.mentra.notes-1.0.12.zip"),
+  require("@assets/miniapps/com.mentra.notes-1.0.13.zip"),
   require("@assets/miniapps/com.mentra.recorder-1.0.6.zip"),
   require("@assets/miniapps/com.mentra.teleprompter-1.0.6.zip"),
   require("@assets/miniapps/com.mentra.translation-1.0.18.zip"),
