4e023fd96c9c7c3572179502e46707949de3478b
Alex Israelov <alex1115alex@gmail.com>
2026-07-22 14:48:32 -0700

    miniapp fixes

--- mobile/src/generated/bundledMiniapps.ts
+++ mobile/src/generated/bundledMiniapps.ts
@@ -6,12 +6,12 @@
 // inside the app binary.
 
 export const BUNDLED_MINIAPPS: number[] = [
-  require("@assets/miniapps/com.mentra.ai-1.4.9.zip"),
+  require("@assets/miniapps/com.mentra.ai-1.4.10.zip"),
   require("@assets/miniapps/com.mentra.captions-1.0.13.zip"),
   require("@assets/miniapps/com.mentra.livestreamer-1.0.7.zip"),
   require("@assets/miniapps/com.mentra.merge-0.1.29.zip"),
   require("@assets/miniapps/com.mentra.navigation-1.1.23.zip"),
-  require("@assets/miniapps/com.mentra.notes-1.0.11.zip"),
+  require("@assets/miniapps/com.mentra.notes-1.0.12.zip"),
   require("@assets/miniapps/com.mentra.recorder-1.0.5.zip"),
   require("@assets/miniapps/com.mentra.teleprompter-1.0.5.zip"),
   require("@assets/miniapps/com.mentra.translation-1.0.17.zip"),
